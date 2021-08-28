80b0314751d9e7c20dea755c26350f55c13ba3f1
Daniela Soto <danisotch@gmail.com>
2021-08-28 14:42:30 -0500

    Added components (shared)

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # RetoRimac
 Implementar Cotizador Seguro Vehicular
+
+Librerias utilizadas:
+    -Ant Design
+    Utilizo esta librería debido a que se asemeja en el UI del prototipado.
+
+# Versionamiento
+    1.  Creación del proyecto ✅
+    2.  Agregación de librerías ✅
+    3.  Creación de componentes ✅
+    4.  Creación de Home
+    5.  Creación de Datos Auto
+    6.  Creación de Arma Plan
+    7.  Creación de Página de Gracias
+    8.  Adaptación de estilos
+    9.  Conexión al API
+    10. Adaptación de datos
+    11. Etapa final
---
+++ cotizador-seguro-vehicular/src/components/shared/Dropdown.tsx
@@ -0,0 +1,25 @@
+import { Select } from 'antd';
+import React, { useState } from 'react';
+
+const { Option } = Select;
+
+interface DdwonProps {
+    optionsList: any;
+    value?: any;
+    onChange?: (event: any) => void;
+    placeholder?: string;
+
+}
+const Dropdown: React.FC<DdwonProps> = (props) => {
+    return (
+        <div>
+            <Select placeholder={props.placeholder} className="select-before" value={props.value} onChange={props.onChange}>
+                {props.optionsList.map((element: any, index: number) => {
+                    return <Option value={element.value} key={element.label + index}>{element.label}</Option>
+                })}
+            </Select>
+        </div>
+    );
+}
+
+export default Dropdown;
